22f32c0aade2459cf57d15f2528eb57be92c70f2
Sheridan <shensh5@oocl.com>
2025-09-05 17:33:30 +0800

    处理跨域

--- src/main/java/com/example/DisneyBookingBackend/controller/HotelController.java
+++ src/main/java/com/example/DisneyBookingBackend/controller/HotelController.java
@@ -1,11 +1,13 @@
 package com.example.DisneyBookingBackend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/hotels")
+@CrossOrigin(origins = "*")
 public class HotelController {
     @GetMapping()
     public String getHotels() {
